9bcabb2d14ad30e4ba8318980c15555aef8aaa09
djlappy <djneo33@yahoo.com>
2019-12-27 14:15:30 -0600

    Add Network Thread

---
+++ app/src/main/java/com/example/wirelesscaliper2/NetworkServer.java
@@ -0,0 +1,39 @@
+package com.example.wirelesscaliper2;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class NetworkServer implements Runnable {
+   byte[] bytes = new byte[30];
+    @Override
+    public void run() {
+        while (true){
+
+            try {
+                DatagramSocket datagramSocket = new DatagramSocket(3333);
+
+            DatagramPacket datagramPacket = new DatagramPacket(bytes,bytes.length);
+           datagramSocket.receive(datagramPacket);
+
+
+
+            for (int i = 0; i < bytes.length; i++) {
+                System.out.println(bytes[i]);
+
+            }
+
+
+
+
+            } catch (SocketException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+}
