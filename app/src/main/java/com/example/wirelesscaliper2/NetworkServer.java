f06d8d1640cd6fbf54876b07ccf2768fb1a8b7b3
djlappy <djneo33@yahoo.com>
2019-12-27 16:19:57 -0600

    Working out Network

--- app/src/main/java/com/example/wirelesscaliper2/NetworkServer.java
+++ app/src/main/java/com/example/wirelesscaliper2/NetworkServer.java
@@ -6,7 +6,7 @@ import java.net.DatagramSocket;
 import java.net.SocketException;
 
 public class NetworkServer implements Runnable {
-   byte[] bytes = new byte[30];
+   byte[] bytes = new byte[8];
    String string;
     @Override
     public void run() {
@@ -16,12 +16,17 @@ public class NetworkServer implements Runnable {
                 DatagramSocket datagramSocket = new DatagramSocket(3333);
 
             DatagramPacket datagramPacket = new DatagramPacket(bytes,bytes.length);
+
            datagramSocket.receive(datagramPacket);
             datagramSocket.close();
-                for (int i = 0; i < bytes.length -1; i++) {
-                    System.out.println(bytes[i]);
+                for (int i = 0; i < bytes.length; i++) {
+                   if (bytes[i] != 0){
+                       string = string+ (char) bytes[i];
+                   }
 
                 }
+                System.out.println(string);
+                string = "";
 
 
 
